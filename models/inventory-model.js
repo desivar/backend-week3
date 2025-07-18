e7dc6e44bcc3e7fab2b441677a1df5055ee4eb78
desivar <jillian_desire@yahoo.es>
2025-07-18 14:33:38 -0600

    fix typo Inventory-model

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -32,11 +32,14 @@ async function getInventoryByClassificationId(classification_id) {
 /* ***************************
  *  Get inventory item by inv_id
  * ************************** */
+/* ***************************
+ *  Get inventory item by inv_id
+ * ************************** */
 async function getInventoryByInventoryId(inventoryId) {
     try {
-        const data = await pool.query
-        `SELECT * FROM public.inventory WHERE inv_id = $1`,
-        [inventoryId]
+        const data = await pool.query(
+            `SELECT * FROM public.inventory WHERE inv_id = $1`,
+            [inventoryId]
         )
         return data.rows
     } catch (error) {
