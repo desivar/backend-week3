1d20a03d8c7d2645f516abb2057f1af2a55935b5
desivar <jillian_desire@yahoo.es>
2025-07-20 22:15:32 -0600

    controllers

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,67 +1,60 @@
-const utilities = require("../utilities/");
-const accountModel = require("../models/account-model");
-
-const accountController = {};
+const utilities = require("../utilities/")
+const accountModel = require("../models/account-model")
 
 /* ****************************************
 *  Deliver login view
 * *************************************** */
-accountController.buildLogin = async (req, res, next) => {
-  res.render("account/login", {
-    title: "Login",
-    nav: await utilities.getNav(),
-    errors: null,
-  });
-};
+async function buildLogin(req, res, next) {
+    let nav = await utilities.getNav()
+    res.render("account/login", {
+      title: "Login",
+      nav,
+      errors: null,
+    })
+  }
 
 /* ****************************************
 *  Deliver registration view
 * *************************************** */
-accountController.buildRegister = async (req, res, next) => {
-  res.render("account/register", {
-    title: "Register",
-    nav: await utilities.getNav(),
-    errors: null,
-  });
-};
+async function buildRegister(req, res, next) {
+    let nav = await utilities.getNav()
+    res.render("account/register", {
+      title: "Register",
+      nav,
+      errors: null,
+    })
+}
 
 /* ****************************************
 *  Process Registration
 * *************************************** */
-accountController.registerAccount = async (req, res) => {
-  const nav = await utilities.getNav();
-  const { account_firstname, account_lastname, account_email, account_password } = req.body;
+async function registerAccount(req, res) {
+  let nav = await utilities.getNav()
+  const { account_firstname, account_lastname, account_email, account_password } = req.body
 
-  try {
-    const regResult = await accountModel.registerAccount(
-      account_firstname,
-      account_lastname,
-      account_email,
-      account_password
-    );
+  const regResult = await accountModel.registerAccount(
+    account_firstname,
+    account_lastname,
+    account_email,
+    account_password
+  )
 
-    if (regResult) {
-      req.flash(
-        "notice",
-        `Congratulations, you're registered ${account_firstname}. Please log in.`
-      );
-      return res.status(201).render("account/login", {
-        title: "Login",
-        nav,
-        errors: null
-      });
-    }
-    
-    throw new Error("Registration failed");
-    
-  } catch (error) {
-    req.flash("notice", "Sorry, the registration failed.");
-    return res.status(501).render("account/register", {
+  if (regResult) {
+    req.flash(
+      "notice",
+      `Congratulations, you\'re registered ${account_firstname}. Please log in.`
+    )
+    res.status(201).render("account/login", {
+      title: "Login",
+      nav,
+    })
+  } else {
+    req.flash("notice", "Sorry, the registration failed.")
+    res.status(501).render("account/register", {
       title: "Registration",
       nav,
-      errors: null,
-    });
+    })
   }
-};
-
-module.exports = accountController;
+}
+  
+module.exports = { buildLogin, buildRegister, registerAccount }
--- controllers/invController.js
+++ controllers/invController.js
@@ -1,140 +1,102 @@
-const invModel = require("../models/inventory-model");
-const utilities = require("../utilities/");
+const invModel = require("../models/inventory-model")
+const utilities = require("../utilities/")
 
-const invCont = {};
+const invCont = {}
 
 /* ***************************
  *  Build inventory by classification view
  * ************************** */
-invCont.buildByClassificationId = async function(req, res, next) {
-  try {
-    const classification_id = req.params.classificationId;
-    const data = await invModel.getInventoryByClassificationId(classification_id);
-    
-    if (!data || data.length === 0) {
-      throw new Error('No inventory found for this classification');
-    }
-
-    const [grid, nav] = await Promise.all([
-      utilities.buildClassificationGrid(data),
-      utilities.getNav()
-    ]);
-
+invCont.buildByClassificationId = async function (req, res, next) {
+    const classification_id = req.params.classificationId
+    const data = await invModel.getInventoryByClassificationId(classification_id)
+    const grid = await utilities.buildClassificationGrid(data)
+    let nav = await utilities.getNav()
+    const className = data[0].classification_name
     res.render("./inventory/classification", {
-      title: `${data[0].classification_name} Vehicles`,
+      title: className + " Vehicles",
       nav,
       grid,
-      errors: null
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+    })
+}
 
 /* ***************************
  *  Build inventory item detail view
  * ************************** */
-invCont.buildByInventoryId = async function(req, res, next) {
-  try {
-    const inv_id = req.params.inventoryId;
-    const data = await invModel.getInventoryByInventoryId(inv_id);
-    
-    if (!data || data.length === 0) {
-      throw new Error('Inventory item not found');
-    }
-
-    const [grid, nav] = await Promise.all([
-      utilities.buildInventoryDisplay(data),
-      utilities.getNav()
-    ]);
-
-    const vehicleName = `${data[0].inv_year} ${data[0].inv_make} ${data[0].inv_model}`;
-    
+invCont.buildByInventoryId = async function (req, res, next) {
+    const inv_id = req.params.inventoryId
+    const data = await invModel.getInventoryByInventoryId(inv_id)
+    const grid = await utilities.buildInventoryDisplay(data)
+    let nav = await utilities.getNav()
+    let d = data[0]
+    const className = d.inv_year + " " + d.inv_make + " " + d.inv_model
     res.render("./inventory/detail", {
-      title: vehicleName,
+      title: className,
       nav,
       grid,
-      errors: null
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+    })
+}
 
 /* *****************************
  *  Build inventory management page
  * **************************** */
-invCont.buildInvManagement = async function(req, res, next) {
-  try {
-    const nav = await utilities.getNav();
-    res.render("./inventory/management", {
-      title: "Inventory Management",
-      nav,
-      errors: null
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+invCont.buildInvManagement = async function (req, res, next) {
+  let nav = await utilities.getNav()
+  res.render("./inventory/management", {
+    title: "Inventory Management",
+    nav,
+    errors: null,
+  })
+}
 
 /* *****************************
  *  Build add classification page
  * **************************** */
-invCont.buildAddClassification = async function(req, res, next) {
-  try {
-    const nav = await utilities.getNav();
-    res.render("./inventory/add-classification", {
-      title: "Add Classification",
-      nav,
-      errors: null
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+invCont.buildAddClassification = async function (req, res, next) {
+  let nav = await utilities.getNav()
+  res.render("./inventory/add-classification", {
+    title: "Add Classification",
+    nav,
+    errors: null,
+  })
+}
 
 /* ****************************************
 *  Process Add Classification
 * *************************************** */
-invCont.processAddNewClassification = async function(req, res, next) {
-  try {
-    const { classification_name } = req.body;
-    const result = await invModel.addNewClassification(classification_name);
+invCont.processAddNewClassification = async function (req, res, next) {
+  const { classification_name } = req.body
+  const result = await invModel.addNewClassification(classification_name)
 
-    if (!result) {
-      throw new Error('Classification addition failed');
-    }
-
-    req.flash('notice', `${classification_name} successfully added.`);
+  console.log("*********** :: " + JSON.stringify(result))
+  if (result) {
+    let nav = await utilities.getNav()
+    req.flash('notice', `${classification_name} successfully added.`)
     res.status(201).render("./inventory/add-classification", {
       title: "Add Classification",
-      nav: await utilities.getNav(),
-      errors: null
-    });
-  } catch (error) {
-    req.flash("notice", "Something went wrong.");
+      nav,
+      errors: [],
+    })
+  } else {
+    let nav = await utilities.getNav()
+    req.flash("notice", "Something went wrong.")
     res.status(501).render("./inventory/add-classification", {
       title: "Add Classification",
-      nav: await utilities.getNav(),
-      errors: null
-    });
+      nav,
+      errors: [],
+    })
   }
-};
+}
 
 /* *****************************
  *  Build add inventory page
  * **************************** */
-invCont.buildAddInventory = async function(req, res, next) {
-  try {
-    const nav = await utilities.getNav();
-    res.render("./inventory/add-inventory", {
-      title: "Add Inventory",
-      nav,
-      errors: null
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+invCont.buildAddInventory = async function (req, res, next) {
+  let nav = await utilities.getNav()
+  res.render("./inventory/add-inventory", {
+    title: "Add Inventory",
+    nav,
+    errors: [],
+  })
+}
 
 module.exports = invCont;
