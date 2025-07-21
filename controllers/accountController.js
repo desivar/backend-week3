69d604b1963dcc569cea3b97488e43132bc7372e
desivar <jillian_desire@yahoo.es>
2025-07-20 21:22:48 -0600

    updated controllers

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,60 +1,67 @@
-const utilities = require("../utilities/")
-const accountModel = require("../models/account-model")
+const utilities = require("../utilities/");
+const accountModel = require("../models/account-model");
+
+const accountController = {};
 
 /* ****************************************
 *  Deliver login view
 * *************************************** */
-async function buildLogin(req, res, next) {
-    let nav = await utilities.getNav()
-    res.render("account/login", {
-      title: "Login",
-      nav,
-      errors: null,
-    })
-  }
+accountController.buildLogin = async (req, res, next) => {
+  res.render("account/login", {
+    title: "Login",
+    nav: await utilities.getNav(),
+    errors: null,
+  });
+};
 
 /* ****************************************
 *  Deliver registration view
 * *************************************** */
-async function buildRegister(req, res, next) {
-    let nav = await utilities.getNav()
-    res.render("account/register", {
-      title: "Register",
-      nav,
-      errors: null,
-    })
-}
+accountController.buildRegister = async (req, res, next) => {
+  res.render("account/register", {
+    title: "Register",
+    nav: await utilities.getNav(),
+    errors: null,
+  });
+};
 
 /* ****************************************
 *  Process Registration
 * *************************************** */
-async function registerAccount(req, res) {
-  let nav = await utilities.getNav()
-  const { account_firstname, account_lastname, account_email, account_password } = req.body
+accountController.registerAccount = async (req, res) => {
+  const nav = await utilities.getNav();
+  const { account_firstname, account_lastname, account_email, account_password } = req.body;
 
-  const regResult = await accountModel.registerAccount(
-    account_firstname,
-    account_lastname,
-    account_email,
-    account_password
-  )
+  try {
+    const regResult = await accountModel.registerAccount(
+      account_firstname,
+      account_lastname,
+      account_email,
+      account_password
+    );
 
-  if (regResult) {
-    req.flash(
-      "notice",
-      `Congratulations, you\'re registered ${account_firstname}. Please log in.`
-    )
-    res.status(201).render("account/login", {
-      title: "Login",
-      nav,
-    })
-  } else {
-    req.flash("notice", "Sorry, the registration failed.")
-    res.status(501).render("account/register", {
+    if (regResult) {
+      req.flash(
+        "notice",
+        `Congratulations, you're registered ${account_firstname}. Please log in.`
+      );
+      return res.status(201).render("account/login", {
+        title: "Login",
+        nav,
+        errors: null
+      });
+    }
+    
+    throw new Error("Registration failed");
+    
+  } catch (error) {
+    req.flash("notice", "Sorry, the registration failed.");
+    return res.status(501).render("account/register", {
       title: "Registration",
       nav,
-    })
+      errors: null,
+    });
   }
-}
-  
-module.exports = { buildLogin, buildRegister, registerAccount }
+};
+
+module.exports = accountController;
--- controllers/baseController.js
+++ controllers/baseController.js
@@ -1,10 +1,29 @@
-const utilities = require("../utilities/")
-const baseController = {}
+const utilities = require("../utilities/");
+const baseController = {};
 
-baseController.buildHome = async function(req, res){
-  const nav = await utilities.getNav()
-  // req.flash("notice", "This is a flash message.")
-  res.render("index", {title: "Home", nav})
-}
+// Base method for all controllers to use
+baseController.buildPage = async function(req, res, view, data = {}) {
+  try {
+    const nav = await utilities.getNav();
+    res.render(view, {
+      ...data,          // Spread any additional data
+      title: data.title || 'Default Title',
+      nav,              // Navigation will be included everywhere
+      errors: null,     // Default error state
+      messages: null    // Default messages
+    });
+  } catch (error) {
+    console.error('Error building page:', error);
+    res.status(500).send('Internal Server Error');
+  }
+};
 
-module.exports = baseController
+// Homepage specific (example)
+baseController.buildHome = async function(req, res) {
+  await this.buildPage(req, res, "index", { 
+    title: "Home" 
+    // Add any home-specific data here
+  });
+};
+
+module.exports = baseController;
