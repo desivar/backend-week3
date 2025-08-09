2a6301505c9ce70d1e7b57b323f121fb40512b72
desivar <jillian_desire@yahoo.es>
2025-08-08 20:07:31 -0600

    account-validation.js

--- utilities/account-validation.js
+++ utilities/account-validation.js
@@ -1,13 +1,13 @@
 const utilities = require(".")
-const accountModel = require("../models/account-model")
 const { body, validationResult } = require("express-validator")
+const accountModel = require("../models/account-model")
+
 const validate = {}
 
 /*  **********************************
  *  Registration Data Validation Rules
-    Unit 4, server-side activity
  * ********************************* */
-validate.registrationRules = () => {
+validate.registationRules = () => {
     return [
       // firstname is required and must be string
       body("account_firstname")
@@ -21,7 +21,7 @@ validate.registrationRules = () => {
         .isLength({ min: 2 })
         .withMessage("Please provide a last name."), // on error this message is sent.
   
-      // valid email is required and cannot already exist in the database
+      // valid email is required and cannot already exist in the DB
       body("account_email")
       .trim()
       .isEmail()
@@ -32,91 +32,76 @@ validate.registrationRules = () => {
         if (emailExists){
           throw new Error("Email exists. Please log in or use different email")
         }
-      }),
-
-      // password is required and must be strong password
-      body("account_password")
-        .trim()
-        .isStrongPassword({
-          minLength: 12,
-          minLowercase: 1,
-          minUppercase: 1,
-          minNumbers: 1,
-          minSymbols: 1,
-        })
-        .withMessage("Password does not meet requirements."),
+      })
     ]
-  }
+}
 
 /*  **********************************
- *  Login Data Validation Rules
-    Unit 4, server-side activity
+ *  Registration Data Validation Rules
  * ********************************* */
-    validate.loginRules = () => {
-        return [
-          // valid email is required and cannot already exist in the database
-          body("account_email")
-          .trim()
-          .isEmail()
-          .normalizeEmail() // refer to validator.js docs
-          .withMessage("A valid email is required."),
-      
-          // password is required and must be strong password
-          body("account_password")
-            .trim()
-            .isStrongPassword({
-              minLength: 12,
-              minLowercase: 1,
-              minUppercase: 1,
-              minNumbers: 1,
-              minSymbols: 1,
-            })
-            .withMessage("Password does not meet requirements."),
-        ]
-      }
+validate.loginRules = () => {
+  return [
+    // valid email is required
+    body("account_email")
+    .trim()
+    .isEmail()
+    .normalizeEmail() // refer to validator.js docs
+    .withMessage("A valid email is required."),
+  
+    // password is required
+    body("account_password")
+      .trim()
+      .isStrongPassword({
+        minLength: 12,
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumbers: 1,
+        minSymbols: 1,
+      })
+      .withMessage("Password is not valid"),
+  ]
+}
 
 /* ******************************
  * Check data and return errors or continue to registration
-   Unit 4, server-side activity
  * ***************************** */
-   validate.checkRegData = async (req, res, next) => {
-    const { account_firstname, account_lastname, account_email } = req.body
-    let errors = []
-    errors = validationResult(req)
-    if (!errors.isEmpty()) {
-      let nav = await utilities.getNav()
-      res.render("account/register", {
-        errors,
-        title: "Registration",
-        nav,
-        account_firstname,
-        account_lastname,
-        account_email,
-      })
-      return
-    }
-    next()
+validate.checkRegData = async (req, res, next) => {
+  const { account_firstname, account_lastname, account_email } = req.body
+  let errors = []
+  errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    let nav = await utilities.getNav()
+    res.render("account/register", {
+      errors,
+      title: "Registration",
+      nav,
+      account_firstname,
+      account_lastname,
+      account_email,
+    })
+    return
   }
+  next()
+}
 
 /* ******************************
- * Check data and return errors or continue to login
-   Unit 4, server-side activity
+ * Check data and return errors or continue to registration
  * ***************************** */
 validate.checkLoginData = async (req, res, next) => {
-    const { account_email } = req.body
-    let errors = []
-    errors = validationResult(req)
-    if (!errors.isEmpty()) {
-      let nav = await utilities.getNav()
-      res.render("account/login", {
-        errors,
-        title: "Login",
-        nav,
-        account_email,
-      })
-      return
-    }
-    next()
+  const { account_email } = req.body
+  let errors = []
+  errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    let nav = await utilities.getNav()
+    res.render("account/login", {
+      errors,
+      title: "Login",
+      nav,
+      account_email,
+    })
+    return
   }
+  next()
+}
 
-module.exports = validate
+module.exports = validate;
