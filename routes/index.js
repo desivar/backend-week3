7c32ca2800e2c8fcd67406b3f9a9ffcf7bff8f2e
desivar <jillian_desire@yahoo.es>
2025-08-13 10:12:43 -0600

    fixed index

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,10 @@ const baseController = require("../controllers/baseController");
 
 // Route to the home page
 router.get("/", baseController.buildHome);
+// Add this to your routes/index.js file
+router.get("/checkout/checkout", (req, res) => {
+    // This tells Express to render the EJS file at views/checkout/checkout.ejs
+    res.render("checkout/checkout");
+});
 
 module.exports = router;
